3279851d12715134145af088382b6aa6296d9ef3
Ikuta-N <2242131e@stu.kobe-u.ac.jp>
2024-11-06 23:35:22 +0900

    map complete!

--- Ikuta_RA/codefile/create_UEAmap.R
+++ Ikuta_RA/codefile/create_UEAmap.R
@@ -1,8 +1,7 @@
 library(tidyverse)
 library(sf)
 
-  
-CZ_2005 <- readr::read_csv("data/2005_original_small-0.001.csv")
+#reading data-----------------------------------------------------------------------------  
 
 ##McEA(中小規模の都市圏)
 
@@ -37,7 +36,7 @@ MEA_2005C <- readr::read_csv("data/MEA2005C.csv", locale = locale(encoding = "cp
                 Employment_Residence_Ratio = 9)
 
 
-#n次郊外を縦につなげる
+#n次郊外を縦につなげる---------------------------------------------------------------------
 
 McEA_sub3 <- McEA_2005 %>% 
   dplyr::select(UEA, 都市圏名, UEA_Name, suburb3, 郊外3, suburb_name3, suburb2) %>% 
@@ -111,12 +110,11 @@ MEA_center <- MEA_2005C %>%
 
 UEA_2005 <- dplyr::bind_rows(McEA_center, McEA_sub1, McEA_sub2, McEA_sub3,
                              MEA_center,  MEA_sub1,  MEA_sub2,  MEA_sub3) %>% 
-  dplyr::select(#-name_j, 
-                -name_e)
+  dplyr::select(-name_j, -name_e)
 
-# base::rm(McEA_center, McEA_sub1, McEA_sub2, McEA_sub3,
-#          MEA_center,  MEA_sub1,  MEA_sub2,  MEA_sub3,
-#          McEA_2005, McEA_2005C, MEA_2005, MEA_2005C)  
+base::rm(McEA_center, McEA_sub1, McEA_sub2, McEA_sub3,
+         MEA_center,  MEA_sub1,  MEA_sub2,  MEA_sub3,
+         McEA_2005, McEA_2005C, MEA_2005, MEA_2005C)
 
 muni_map <- sf::read_sf("data/mmm20051001/mmm20051001.shp", options = "ENCODING=CP932") %>% 
   tidyr::replace_na(replace = list(GNAME = ""))
@@ -128,4 +126,37 @@ UEA_2005.sf <- muni_map %>%
   dplyr::mutate(JISCODE = dplyr::if_else(GNAME %in% c("川崎市", "福岡市"), JISCODE + 30, JISCODE)) %>% 
   dplyr::full_join(UEA_2005 , by = "JISCODE")
 
+#creating map---------------------------------------------------------
 
+UEA_2005.sf %>% 
+  ggplot2::ggplot() + 
+  ggplot2::geom_sf(aes(fill = 都市圏名)) +
+  ggplot2::theme_minimal() +
+  ggplot2::theme(legend.position = "none") +
+  ggplot2::coord_sf(datum = NA) +
+  ggplot2::labs(title = "UEA")　-> UEAmap_2005
+
+UEA_2005.sf %>% 
+  ggplot2::ggplot() + 
+  ggplot2::geom_sf(aes(fill = 都市圏名)) +
+  ggplot2::theme_minimal() +
+  ggplot2::theme(legend.position = "none") +
+  ggplot2::coord_sf(ylim = c(34, 37),
+                    xlim = c(138, 141),
+                    datum = NA) +
+  ggplot2::labs(title = "UEA")　-> UEAmap_2005_Kanto
+
+
+# UEA_2005.sf %>% 
+#   dplyr::mutate(UEA = base::as.factor(UEA)) %>% 
+#   dplyr::select(UEA) %>% 
+#   plot()
+# 
+# library(mapview)
+# UEA_2005.sf %>% 
+#   dplyr::mutate(UEA = str_replace_all(as.character(UEA), c(
+#     "1" = "A",
+#     "2" = "B",
+#     "3" = "C"
+#    ))) %>% 
+#   mapview(zcol = "UEA")
--- Ikuta_RA/codefile/reprecation_CZmap.R
+++ Ikuta_RA/codefile/reprecation_CZmap.R
@@ -2,25 +2,40 @@ library(tidyverse)
 library(sf)
 #DropboxからDLしたデータはすべてdataフォルダに格納
 
-CZ_data <- readr::read_csv("data/2005_original_small-0.001.csv") %>% 
+CZ_2005 <- readr::read_csv("data/2005_original_small-0.001.csv") %>% 
   dplyr::rename(JISCODE = i)
 
 muni_map <- sf::read_sf("data/mmm20051001/mmm20051001.shp", options = "ENCODING=CP932") %>% 
-  dplyr::left_join(CZ_data, by = "JISCODE")                                                  
+  dplyr::left_join(CZ_2005, by = "JISCODE")                                                  
 
+temp <- muni_map %>% 
+  dplyr::tibble() %>% 
+  select(cluster, NAME) %>% 
+  dplyr::group_by(cluster) %>% 
+  dplyr::slice_head(n = 1) %>% 
+  dplyr::rename(rep = NAME)
 
 muni_map %>% 
-  dplyr::mutate(cluster = as.factor(cluster)) %>% 
-  dplyr::filter(JISCODE %in% (5000:5999)) %>%
-  dplyr::select(cluster) %>% 
+  dplyr::left_join(temp, by = "cluster") %>% 
   ggplot2::ggplot() +
-  ggplot2::geom_sf(aes(fill = cluster)) +
+  ggplot2::geom_sf(aes(fill = rep)) +
   ggplot2::theme_bw() +
   # ggplot2::scale_fill_brewer(type = "qua") +
   ggplot2::theme(legend.position = "none") +
   ggplot2::coord_sf(datum = NA) +
-  ggplot2::labs(title = "Commuting Zone")
+  ggplot2::labs(title = "Commuting Zone") -> CZmap_2005
 
+muni_map %>% 
+  dplyr::left_join(temp, by = "cluster") %>% 
+  ggplot2::ggplot() +
+  ggplot2::geom_sf(aes(fill = rep)) +
+  ggplot2::theme_bw() +
+  # ggplot2::scale_fill_brewer(type = "qua") +
+  ggplot2::theme(legend.position = "none") +
+  ggplot2::coord_sf(ylim = c(34, 37),
+                    xlim = c(138, 141),
+                    datum = NA) +
+  ggplot2::labs(title = "Commuting Zone") -> CZmap_2005_Kanto
 
 
 
