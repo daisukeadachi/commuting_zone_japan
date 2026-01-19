f150976a15e8644c304f96d9b9d13920b3c720eb
Ikuta-N <t.ikuta.naoki@gmail.com>
2026-01-19 22:37:56 +0900

    the processes of making kanto maps and whole maps into one code

--- Ikuta_RA/codefile/tree-height_original.R
+++ Ikuta_RA/codefile/tree-height_original.R
@@ -18,13 +18,12 @@ plan(multicore, workers = n_core)
 
 
 "%not.in%" <- Negate("%in%")
-colors <- RColorBrewer::brewer.pal(5, "Set2")
-# c("#66C2A5", "#FC8D62", "#8DA0CB", "#E78AC3", "#A6D854", "#FFD92F", "#E5C494", "#B3B3B3")
-# limitation range
+colors <- c(RColorBrewer::brewer.pal(5, "Set2"), "#377EB8")
 
+# limitation range
 lim <- list(x = c(129.3, 142.3), y = c(31.2, 42))
+lim_kanto <- list(x = c(138, 140.9), y = c(34.7, 37.1))
 width <- .1
-outdir <- "output/map_image/CZ2015/"
 
 
 # directory to persist signature->color maps across runs
@@ -35,6 +34,7 @@ czlist.all <- list.files(path = "output/clustered/original", full.names = TRUE, 
 year <- seq(1980, 2015, 5) |> as.character()
 
 IMGDIR <- "output/map_image/tree-height/original/"
+IMGDIR_kanto <- "output/map_image/tree-height/original_kanto/"
 iwalk(czlist.all, ~ {
   folder <- dirname(.x) |> stringr::str_remove("output/clustered/original/") 
   dir.create(paste0(IMGDIR, folder), recursive = TRUE, showWarnings = FALSE)
@@ -64,7 +64,7 @@ walk(year, function(YEAR) {
     Gohunai <- CZ.sf$cluster[EdoCenter]
     CZ_color <- assign_group_colors(CZ.sf, "cluster", colors = colors, fixed = list(value = Gohunai, color = RColorBrewer::brewer.pal(6, "Set2")[6]))
     CZ.sf <- dplyr::left_join(CZ.sf, CZ_color, by = "cluster") %>%
-      dplyr::select(geometry, color) %>% 
+      dplyr::select(geometry, color, JISCODE) %>% 
       dplyr::mutate(color = dplyr::if_else(
         is.na(color), "#a9a9a9", color
       ))
@@ -82,6 +82,20 @@ walk(year, function(YEAR) {
       device = "png",
       bg = "white"
     )
+    rm(enl)
+    CZ.sf %>%
+      dplyr::filter(JISCODE %in% (7000:23999)) %>%
+      make_basic_plot(
+        lim_x = lim_kanto$x,
+        lim_y = lim_kanto$y,
+        HokkaidoLine = FALSE
+      ) -> kanto
+    ggplot2::ggsave(kanto,
+      filename = paste0(IMGDIR_kanto, folder, "/", .y, ".png"),
+      device = "png",
+      bg = "white"
+    )
+    
   },.options = furrr::furrr_options(seed = TRUE),
   .progress = TRUE
   )
